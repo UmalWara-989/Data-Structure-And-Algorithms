182fa72bcfa4233a3a59adb0a06abc2d7aebfd7a
UMAL WARA <umalwaraarain123786@gmail.com>
2026-03-14 13:27:13 +0500

    Implement palindrome check for linked list
    Added methods to check if a linked list is a palindrome, including finding the middle point and reversing the second half of the list.

--- Linkedlist_Problem8.java
+++ Linkedlist_Problem8.java
@@ -1,3 +1,6 @@
+// Q:6 Check if a linked list is Palindrome
+// Time complexity = O(n) and Space complexity = O(1)
+
 class Linkedlist_Problem8 {
 Node head;
 private String size;
@@ -52,10 +55,66 @@ public void Print_list() {
   public int Get_Size(){
     return size;
     }
+
+// Actual Solution  
+// Supporting methods
+public Node Middle_Point(Node head){
+ Node hare = head;
+ Node turtle = head;
+ while (hare.next!=null && hare.next.next!=null){
+   hare = hare.next.next;
+   turtle = turtle.next;
+    }
+    return turtle;
+}
+public Node Reverse(Node head){
+  Node prev = null;
+  Node current = head;
+  
+  while (current!=null){
+    Node Next = current.next;
+    current.next = prev;
+    prev = current;
+    current = Next;
+    }
+    return prev;
+}
+  
+public boolean Palindrome(Node head){
+// Corner case
+  if (head==null || head.next==null){
+    return true;
+    }
+  Node middle = Middle_Point(head);
+  Node End_2nd_Hallf = Reverse(middle.next);
+  Node Start_1st_Half = head;
+  while (End_2nd_Hallf!=null){
+  if (Start_1st_Half.data!=End_2nd_Hallf.data){
+    return false;
+        }
+  Start_1st_Half = Start_1st_Half.next;
+  End_2nd_Hallf = End_2nd_Hallf.next;
+    }
+  return true;
+}  
   
  //Main method
 public static void main(String[] args){
+// Q:6 Solution
+Linked_List_Advanced_Questions l3 = new Linked_List_Advanced_Questions();
   
+l3.Add_Last("A");
+l3.Add_Last("B");
+l3.Add_Last("C");
+l3.Add_Last("D");
+l3.Add_Last("E");
+l3.Add_Last("F");
+  
+System.out.println("\"Q:6\"");
+System.out.println("\"List\"");
+l3.Print_list();
+  
+System.out.println("Cycle exist in list (Status) : "+l3.Cycle(l3.head)); 
  
 }
 }
